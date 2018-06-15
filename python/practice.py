683761dbe4368b2a613d186f39abe4e7a9821167
KevinFarthing <farthing.kw@gmail.com>
2018-06-15 14:39:19 -0500

    refactored hangman to use class Object

--- python/practice.py
+++ python/practice.py
@@ -2,55 +2,64 @@
 from random import randint
 from modules import word_list
 
-def word_select():
-        return word_list[randint(0,len(word_list)-1)]
+class Hangman:
+    def __init__(self, word="", lives=6, status=[],wrong=[]):
+        self.lives = lives
+        self.word = word
+        self.status = status
+        self.wrong = wrong
 
-def guess_check(word, status, lives,wrong):
-    guess = input("Guess a letter: ")
-    while len(guess)>1:
-        guess = input("Guess ONE letter: ").lower()
-    if guess in word.lower():
-        print(f"Awesome! {guess} is in the word!")
-        for i in range(len(word)):
-            if word[i].lower() == guess:
-                status[i] = word[i]
-    else:
-        lives -= 1
-        print(f"Sorry, {guess} was not in the word.")
-        wrong.append(guess)
-#     print_status(status,lives,wrong)
-    return word, status, lives, wrong
+    # def word_select():
+    #         return word_list[randint(0,len(word_list)-1)]
 
-def print_status(status,lives,wrong):
-    # clear_output()
-    print("***************")
-    print(f"Lives: {lives}")
-    print(f"Past Guesses: ", end="")
-    print(" ".join(wrong))
-    print(" ".join(status))
+    def guess_check(self):
+        guess = input("Guess a letter: ")
+        while len(guess)>1:
+            guess = input("Guess ONE letter: ").lower()
+        if guess in self.word.lower():
+            print(f"Awesome! {guess} is in the word!")
+            for i in range(len(self.word)):
+                if self.word[i].lower() == guess:
+                    self.status[i] = self.word[i]
+        else:
+            self.lives -= 1
+            print(f"Sorry, {guess} was not in the word.")
+            self.wrong.append(guess)
+    #     print_status(status,lives,wrong)
+        # return status, lives, wrong
 
-def game_function():
-    word = word_select()
-    lives = 6
-    wrong = []
-    status = list("_"*len(word))
-    while lives != 0:
-        print_status(status,lives,wrong)
-        word, status, lives, wrong = guess_check(word, status, lives, wrong)
-        if "_" not in status:
-            print("You Win!")
-            print(f"The word was {word}")
-            return
-    print(f"Game Over! The correct word was {word}!")
+    def print_status(self):
+        # clear_output()
+        print("***************")
+        print(f"Lives: {self.lives}")
+        print(f"Past Guesses: ", end="")
+        print(" ".join(self.wrong))
+        print(" ".join(self.status))
 
-def main():
-    while True:
-        game_function()
-        cont = input("Would you like to play again? y/n ").lower()
-        if cont != "y":
-            print("Thanks for playing!")
-            break
+    def game_function(self):
+        self.lives = 6
+        self.wrong = []
+        while self.lives != 0:
+            self.print_status()
+            self.guess_check()
+            if "_" not in self.status:
+                print("You Win!")
+                print(f"The word was {self.word}")
+                return
+        print(f"Game Over! The correct word was {self.word}!")
 
+    def main(self):
+        while True:
+            self.word = word_list[randint(0,len(word_list)-1)]
+            self.status = list("_"*len(self.word))
+            self.game_function()
+            cont = input("Would you like to play again? y/n ").lower()
+            if cont != "y":
+                print("Thanks for playing!")
+                break
 
               
-main()
+# game = Hangman()
+# game.main()
+
+Hangman().main()
